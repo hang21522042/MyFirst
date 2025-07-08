6834053d78ce49934cc390839eed502b30e9b569
UIT-21522042 <ngthuyhang12122003@gmail.com>
2025-07-08 11:02:43 +0700

    commit

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+//public/ private/protective
+public class Hello {
+	// main ctrl space enter
+	public static void main(String[] args) {
+		//sysout ctrl space: print on screen
+		System.out.println("Hello");
+	}
+}
