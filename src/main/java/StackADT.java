0d49719f531ea57d9e592fabcf884c4159978e91
UIT-21522042 <ngthuyhang12122003@gmail.com>
2025-07-08 10:40:21 +0700

    tesstt

---
+++ src/main/java/StackADT.java
@@ -0,0 +1,14 @@
+public interface StackADT <T>{
+
+    public void push(T item);
+    //trar về và loại bo phan tu
+    public T pop();
+    //Lay ra va khong loai bo
+    public T peek();
+    //Ktra rong
+    public boolean isEmpty();
+    //kich thuoc
+    public int size();
+    //Chuoi
+    public String toString();
+}
